121cc257ebff25a7e27e220720652d7dea4854c9
Orlndo33 <joseapaza33@gmail.com>
2019-09-04 15:44:12 -0300

    Agregado de Curso y alumno/ pendiente borrar sus data notacio

---
+++ DominioSecretaria/ADO/ContextConfiguracion/AlumnoConfiguracion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DominioSecretaria.Escuela;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DominioSecretaria.ADO.ContextConfiguracion
+{
+    public class AlumnoConfiguracion : IEntityTypeConfiguration<Alumno>
+    {
+        public void Configure(EntityTypeBuilder<Alumno> mb)
+        {
+            mb.ToTable("Alumno");
+
+            mb.HasKey(a => a.legajo);
+
+            mb.Property<int>("idPersona");
+            mb.HasOne(a => a.Persona)
+                .WithMany()
+                .HasForeignKey("idPersona")
+                .IsRequired();
+
+            mb.Property<byte>("idCursoActual");
+            mb.HasOne(a => a.CursoActual)
+                .WithMany()
+                .HasForeignKey("idCursoActual")
+                .IsRequired();
+
+            mb.Property(a => a.legajo)
+                .HasColumnName("legajo");
+
+            mb.Property(a => a.Libro)
+                .HasColumnName("libro");
+
+            mb.Property(a => a.Folio)
+                .HasColumnName("folio");
+        }
+    }
+}
